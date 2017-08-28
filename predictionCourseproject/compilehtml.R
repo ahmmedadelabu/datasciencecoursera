bb47e334e1ca9563f2a4062d7e436de69ee080df
Adelabu Ayinla Ahmmed <ahmmedadelabu@yahoo.com>
2017-08-28 07:10:53 +0100

    prediction and machine learning course project

--- predictionCourseproject/compilehtml.R
+++ predictionCourseproject/compilehtml.R
@@ -1,4 +1,4 @@
 
 library(rmarkdown)
 setwd('C:\\Users\\Ahmmed\\Desktop\\from flash drive 25082017\\cousera\\course material\\praticalmachine\\wk4\\')
-render("wk4_corseproject.R","html_document")
+render("courseproject.R","html_document")
